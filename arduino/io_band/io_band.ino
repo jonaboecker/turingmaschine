ce3543df9ba80c541984f0400d500ddeab7c72a4
ThatsNotMe <mineflax@gmail.com>
2024-12-02 18:08:37 +0100

    Prototyping with graphite resistor

--- arduino/io_band/io_band.ino
+++ arduino/io_band/io_band.ino
@@ -8,15 +8,43 @@
 
 #define DELAY_INTERVAL 250  // 250ms pause between each pixel
 
+struct SensorData {
+  int sensor_val; // Key
+  int pos;        // Position
+  int state;      // State
+};
+
+const int VOLTAGE_SENSOR_PIN = A0;
+
+float voltage = 0;
+
+const int delta = 100;
+
+SensorData stripe_dict[] = {
+  {700, 0, 0},
+  {200, 1, 0},
+};
+
+const int dictSize = sizeof(stripe_dict) / sizeof(stripe_dict[0]);
+
 Adafruit_NeoPixel WS2812B(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   WS2812B.begin();  // INITIALIZE WS2812B strip object (REQUIRED)
+
+  Serial.begin(9600);
 }
 
 void loop() {
+  int sensor_val = read_voltage();
+  //Serial.println(sensor_val);
+  connect_led_demo(sensor_val);
+  //led_demo();
+}
+
+void led_demo() {
   WS2812B.clear();  // set all pixel colors to 'off'. It only takes effect if pixels.show() is called
-  WS2812B.setBrightness(100); // a value from 0 to 255
+  WS2812B.setBrightness(10); // a value from 0 to 255
 
   // turn pixels to green one by one with delay between each pixel
   for (int pixel = 0; pixel < NUM_PIXELS; pixel++) {         // for each pixel
@@ -37,5 +65,57 @@ void loop() {
     WS2812B.show();
 
     delay(DELAY_INTERVAL);  // pause between each pixel
+  }
+}
+
+int read_voltage() {
+  int sensorValue = analogRead(VOLTAGE_SENSOR_PIN);
+  /*
+  float voltage = sensorValue * (5/1024);
+  Serial.print("Spannung: ");
+  Serial.println(voltage);
+  */
+  delay(50);
+  return sensorValue;
+}
+
+void connect_led_demo(int val) {
+  WS2812B.setBrightness(10); // a value from 0 to 255
+
+  for (int i = 0; i < dictSize; i++) {
+    int threshold = stripe_dict[i].sensor_val;
+    if (val >= threshold - delta && val <= threshold + delta) {
+
+      /*
+      Serial.print("Threashold ");
+      Serial.print(stripe_dict[i].sensor_val);
+      Serial.print(" is greater or equal to ");
+      Serial.println(val);
+      */
+
+      int state = stripe_dict[i].state;
+      int pixel = stripe_dict[i].pos;
+      if (state == 0){
+        WS2812B.setPixelColor(pixel, WS2812B.Color(255, 0, 0));
+      } else if (state == 1){
+        WS2812B.setPixelColor(pixel, WS2812B.Color(0, 255, 0));
+      } else if (state == 2){
+        WS2812B.setPixelColor(pixel, WS2812B.Color(0, 0, 255));
+      } else if (state == 3){
+        WS2812B.setPixelColor(pixel, WS2812B.Color(0, 0, 0));
+      }
+
+      
+      Serial.print("Set Pixel ");
+      Serial.print(pixel);
+      Serial.print(" to state ");
+      Serial.println(state);
+
+      
+      stripe_dict[i].state += 1;
+      stripe_dict[i].state = stripe_dict[i].state % 4;
+      WS2812B.show();
+      delay(100);
+    }
   }
 }
