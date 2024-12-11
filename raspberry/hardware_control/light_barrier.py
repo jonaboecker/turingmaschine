385d83e1c8b909509c8a948a32bbda3d55cb7d1a
lara00 <lara@geyer-rv.de>
2024-12-11 21:07:52 +0100

    Add light barrier sensor logic in light_barrier.py

--- raspberry/hardware_control/light_barrier.py
+++ raspberry/hardware_control/light_barrier.py
@@ -1,15 +1,29 @@
 """
-This module contains the Functions to control the Light Barrier.
+This module contains the functions to control the Light Barrier.
 """
 
-# pylint: todo: disable=fixme
+import platform
+
+# Import RPi.GPIO (just supported on RPi) or fake_rpi.RPi.GPIO based on the platform
+import importlib
+
+if platform.system() == "Linux":
+    GPIO = importlib.import_module("RPi.GPIO")
+else:
+    GPIO = importlib.import_module("fake_rpi.RPi.GPIO")
+
+# GPIO pin for the light barrier
+SENSOR_PIN = 17
+
+# Initialize GPIO
+GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
+GPIO.setup(SENSOR_PIN, GPIO.IN)  # Configure the pin as an input
 
 def get_state():
     """
     Get the state of the light barrier.
 
     Returns:
-        bool: The state of the light barrier (True if blocked, False if not blocked).
+        bool: state The of the light barrier (True if blocked, False if not blocked).
     """
-    # todo: implement the state detection
-    return True
+    return GPIO.input(SENSOR_PIN) == 1
