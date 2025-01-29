2a6282dce6b3e8dc413728e3d583446fe8b3d554
ThatsNotMe <mineflax@gmail.com>
2025-01-29 10:40:39 +0100

    push assets technical docu

--- arduino/steppermotor_example/steppermotor_example.ino
+++ arduino/steppermotor_example/steppermotor_example.ino
@@ -9,8 +9,8 @@ int MS2 = 6; //GPIO6 in Arduino UNO --- MS2 for A4988
 int MS3 = 5; //GPIO5 in Arduino UNO --- MS3 for A4988
 
 //Motor Specs
-const int spr = 500; //Steps per revolution
-int RPM = 100; //Motor Speed in revolutions per minute
+const int spr = 200; //Steps per revolution
+int RPM = 200; //Motor Speed in revolutions per minute
 int Microsteps = 1; //Stepsize (1 for full steps, 2 for half steps, 4 for quarter steps, etc)
 
 //Providing parameters for motor control
@@ -30,5 +30,5 @@ void setup() {
 
 void loop() {
     digitalWrite(Sleep, HIGH); //A logic high allows normal operation of the A4988 by removing from sleep
-    stepper.rotate(720);
+    stepper.rotate(360);
 }
