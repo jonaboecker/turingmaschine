141f685f52c340073a28a751c8bc5c8551eceb1f
TobiasReyEye <tobi-stoehr@t-online.de>
2025-01-29 17:30:25 +0100

    adjusted code for testing purposes

--- raspberry/hardware_control/light_barrier.py
+++ raspberry/hardware_control/light_barrier.py
@@ -7,7 +7,7 @@ import platform
 # Import RPi.GPIO (just supported on RPi) or fake_rpi.RPi.GPIO based on the platform
 import importlib
 
-if platform.system() == "Linux":
+if platform.system() != "Linux":
     GPIO = importlib.import_module("RPi.GPIO")
 else:
     GPIO = None
@@ -16,7 +16,7 @@ else:
 SENSOR_PIN = 17
 
 # Initialize GPIO
-if platform.system() == "Linux":
+if platform.system() != "Linux":
     GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
     GPIO.setup(SENSOR_PIN, GPIO.IN)  # Configure the pin as an input
 
@@ -27,6 +27,6 @@ def get_state():
     Returns:
         bool: state The of the light barrier (True if blocked, False if not blocked).
     """
-    if platform.system() == "Linux":
+    if platform.system() != "Linux":
         return GPIO.input(SENSOR_PIN) == 1
     return True # Return True for testing on non-Raspberry Pi platforms
