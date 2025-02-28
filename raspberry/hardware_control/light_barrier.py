8546519fb3155c4f0942dd2fdff288dd30e9ced9
lara00 <lara@geyer-rv.de>
2025-02-28 04:00:49 +0100

    Ersetze RPi.GPIO durch libgpiod für Raspberry Pi 5

--- raspberry/hardware_control/light_barrier.py
+++ raspberry/hardware_control/light_barrier.py
@@ -4,21 +4,20 @@ This module contains the functions to control the Light Barrier.
 
 import platform
 
-# Import RPi.GPIO (just supported on RPi) or fake_rpi.RPi.GPIO based on the platform
-import importlib
-
 if platform.system() == "Linux":
-    GPIO = importlib.import_module("RPi.GPIO")
+    import gpiod
 else:
-    GPIO = None
+    GPIOD = None
 
 # GPIO pin for the light barrier
 SENSOR_PIN = 17
+CHIP = "/dev/gpiochip4"  # Standard GPIO-Chip auf dem Raspberry Pi 5
 
 # Initialize GPIO
 if platform.system() == "Linux":
-    GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
-    GPIO.setup(SENSOR_PIN, GPIO.IN)  # Configure the pin as an input
+    chip = gpiod.Chip(CHIP)
+    line = chip.get_line(SENSOR_PIN)
+    line.request(consumer="light_barrier", type=gpiod.LINE_REQ_DIR_IN)
 
 def get_state():
     """
@@ -28,5 +27,5 @@ def get_state():
         bool: state The of the light barrier (True if blocked, False if not blocked).
     """
     if platform.system() == "Linux":
-        return GPIO.input(SENSOR_PIN) == 1
-    return True # Return True for testing on non-Raspberry Pi platforms
+        return line.get_value() == 1
+    return True  # Simulierter Wert für Nicht-Raspberry-Pi-Systeme
