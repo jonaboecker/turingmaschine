72c3d319c9124f4903d934dd06c6edc7ba5d5f05
Jona Böcker <90841615+jonaboecker@users.noreply.github.com>
2025-03-10 13:03:22 +0100

    show logs in startup script

--- raspberry/assets/startup.sh
+++ raspberry/assets/startup.sh
@@ -10,3 +10,4 @@ chromium-browser --kiosk http://localhost:5000 &
 
 # Start Container at Port 5000
 sudo docker run --privileged --device /dev/gpiochip4 -v /dev/ttyACMO:/dev/ttyACMO -v /dev/gpiomem:/dev/gpiomem -v /sys:/sys -v /proc/device-tree:/proc/device-tree -p 5000:5000 flask-app:latest
+lxterminal -e "bash -c 'sudo docker logs -f $(sudo docker ps -q -f ancestor=flask-app:latest); exec bash'" &
